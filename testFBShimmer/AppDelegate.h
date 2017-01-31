3a84a859b6cc987393e494a6a01bd8d946101695
AgustinDeLeon <adeleon@codigodelsur.com>
2017-01-31 19:42:01 -0300

    Initial Commit

---
+++ testFBShimmer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFBShimmer
+//
+//  Created by Agustin De Leon on 31/1/17.
+//  Copyright © 2017 Agustin De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testFBShimmer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testFBShimmer
+//
+//  Created by Agustin De Leon on 31/1/17.
+//  Copyright © 2017 Agustin De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
